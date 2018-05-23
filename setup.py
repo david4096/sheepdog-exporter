148a7ee50d54dce297fedad92c8a9a73ad15e78a
David Steinberg <david@resium.com>
2018-05-22 22:07:24 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Topic :: Scientific/Engineering :: Bio-Informatics',
     ],
-    version='0.1.2',
+    version='0.1.3',
     keywords=['genomics', 'metadata', 'NIHDataCommons'],
     # Use setuptools_scm to set the version number automatically from Git
 )
